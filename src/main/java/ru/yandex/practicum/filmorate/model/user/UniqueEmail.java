4975276af5f980134491a619d37a22487770603c
Egor Malyshev <ya.krumm@ya.ru>
2025-05-03 19:08:24 +0300

    sprint-10 ready to reviwe

--- src/main/java/ru/yandex/practicum/filmorate/model/user/UniqueEmail.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/user/UniqueEmail.java
@@ -5,15 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import jakarta.validation.Constraint;
-import jakarta.validation.ConstraintValidator;
-import jakarta.validation.ConstraintValidatorContext;
 import jakarta.validation.Payload;
-import lombok.extern.slf4j.Slf4j;
-import ru.yandex.practicum.filmorate.storage.impl.UsersStorageService;
 
+/*
+* Аннотация @UniqueEmail связана с email-списком класса UserStorageService
+*/
 @Target({ ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = UniqueEmailValidator.class)
@@ -26,25 +23,4 @@ public @interface UniqueEmail {
 	Class<?>[] groups() default {};
 
 	Class<? extends Payload>[] payload() default {};
-}
-
-/*
- * Аннотация @UniqueEmail связана с email-списком класса UserStorageService
- */
-@Slf4j
-class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
-
-	@Autowired
-	UsersStorageService userStorageService;
-
-	@Override
-	public boolean isValid(String value, ConstraintValidatorContext context) {
-
-		if (userStorageService.getEmailList().contains(value)) {
-			log.warn("Пользователь с таким email уже зарегестрирован");
-			return false;
-		} else {
-			return true;
-		}
-	}
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/user/UniqueEmailValidator.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.filmorate.model.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.extern.slf4j.Slf4j;
+import ru.yandex.practicum.filmorate.storage.impl.UsersStorageService;
+
+/*
+ * Аннотация @UniqueEmail связана с email-списком класса UserStorageService
+ */
+@Slf4j
+class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
+
+	@Autowired
+	UsersStorageService userStorageService;
+
+	@Override
+	public boolean isValid(String value, ConstraintValidatorContext context) {
+
+		if (userStorageService.getEmailList().contains(value)) {
+			log.warn("Пользователь с таким email уже зарегестрирован");
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
